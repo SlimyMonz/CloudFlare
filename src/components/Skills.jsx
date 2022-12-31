effc133dad95e288407ca5fc12e6c7b9fc6fdb08
monz <harry_hocker@icloud.com>
2022-12-30 20:52:48 -0500

    Updated "Skills.jsx" to have more simplified code.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,36 +1,29 @@
-import { useAppContext } from "../appContext";
-import { Element } from "react-scroll";
+import {Element} from "react-scroll";
 // Data
-import { skillData} from "../data";
+import {skillData} from "../data";
 // Components
-import { Col, Container, Row } from "react-bootstrap";
-import { Title } from "./globalStyledComponents";
+import {Col, Container, Row} from "react-bootstrap";
+import {Title} from "./globalStyledComponents";
 
 export default function Skills() {
-  const { } = useAppContext();
-
-  return (
-    <Element name={"Skills"} id="skills">
-      <section className="section">
-        <Container className="text-center">
-          <Title>
-            <h2>Skills</h2>
-            <div className="underline"></div>
-          </Title>
-          <Row className="mt-3 align-items-center">
-            {skillData.map((skills) => {
-              return (
+  return <><Element name={"Skills"} id="skills">
+    <section className="section">
+      <Container className="text-center">
+        <Title>
+          <h2>Skills</h2>
+          <div className="underline"></div>
+        </Title>
+        <Row className="mt-3 align-items-center">
+          {skillData.map(skills => (
                 <Col xs={4} key={skills.id} className="my-md-5">
                   <figure>
                     {skills.skill}
                     <figcaption>{skills.name}</figcaption>
                   </figure>
                 </Col>
-              );
-            })}
-          </Row>
-        </Container>
-      </section>
-    </Element>
-  );
+            ))}
+        </Row>
+      </Container>
+    </section>
+  </Element></>;
 }
